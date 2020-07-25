403f427eb5a3a0d87fe78afb835ea87ef326b4e8
Martin RATH <martin.rath@gmail.com>
2020-07-25 11:58:49 +0800

    Unique Stations

--- RBN-Stats.py
+++ RBN-Stats.py
@@ -1,3 +1,9 @@
 import pandas as pd
 import requests
 
+df = pd.read_csv('20200712.zip')
+print(df.head(5))
+print(df.info())
+Skimmers_call = df['callsign'].unique()
+print(Skimmers_call)
+print('They were', len(Skimmers_call),'Skimmer stations online today on the RBN' )
